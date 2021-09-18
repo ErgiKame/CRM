4a2276534e8963463ac2b73bd2331a176d4857ce
ErgiKame <ergikame@gmail.com>
2021-09-18 13:05:39 +0200

    added static files configs

--- CRM/urls.py
+++ CRM/urls.py
@@ -13,12 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import lead_list, landing_page, LandingPageView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
+
+# This is how we reference static files using settings values that we set on settings.py
+# In production we upload static files on Amazon S3 or other service providers
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
