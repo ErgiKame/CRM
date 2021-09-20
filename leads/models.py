08d2a0ae5fa1b62e1fd299a751af0bdf01a12ce0
ErgiKame <ergikame@gmail.com>
2021-09-20 11:55:15 +0200

    added a signal

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -40,7 +41,16 @@ class Lead(models.Model):
 class Agent(models.Model):
     # Every agent has its own user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=None)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
+# In this case instance is User that is being sent. Created = tells us if model was created. False if its just saved.
+def post_user_created_signal(sender, instance, created, **kwargs):
+    print(instance, created)
+    if created:
+        UserProfile.objects.create(user=instance)
+
+# Once we receive event from User, we use post_user_created_signal to handle it
+post_save.connect(post_user_created_signal, sender=User)
